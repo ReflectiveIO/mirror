179ebd15af8d492edf6c5fefac59091173a4b914
Eguo Wang <wangeguo@gmail.com>
2021-07-12 09:25:20 +0800

    arch glass and matte materials

---
+++ src/slg/material/matte.rs
@@ -0,0 +1,96 @@
+use crate::rays::color::Spectrum;
+use crate::rays::geometry::Vector;
+use crate::rays::Properties;
+use crate::slg::bsdf::hitpoint::HitPoint;
+use crate::slg::image_map::ImageMapCache;
+use crate::slg::material::material::MaterialTrait;
+use crate::slg::textures::Texture;
+
+pub struct MatteMaterial {
+    kd: Texture,
+}
+
+impl MatteMaterial {
+    pub fn new(
+        front_transp: &Texture,
+        back_transp: &Texture,
+        emitted: &Texture,
+        bump: &Texture,
+        col: &Texture,
+    ) -> Self {
+        MatteMaterial {
+            kd: Texture::default(),
+        }
+    }
+
+    pub fn get_kd(&self) -> &Texture {
+        &self.kd
+    }
+}
+
+impl MaterialTrait for MatteMaterial {
+    fn get_type(&self) -> MaterialType {
+        MaterialType::Matte
+    }
+
+    fn get_event_types(&self) -> BSDFEvent {
+        todo!()
+    }
+
+    fn albedo(&self, hit_point: &HitPoint) -> Spectrum {
+        todo!()
+    }
+
+    fn evaluate_total(&self, hit_point: &HitPoint) -> Spectrum {
+        todo!()
+    }
+
+    fn evaluate(
+        &self,
+        hp: &HitPoint,
+        local_light_dir: &Vector,
+        local_eye_dir: &Vector,
+        event: &BSDFEvent,
+        direct_pd_fw: Option<f32>,
+        reverse_pd_fw: Option<f32>,
+    ) -> Spectrum {
+        todo!()
+    }
+
+    fn sample(
+        &self,
+        hp: &HitPoint,
+        local_fixed_dir: &Vector,
+        local_sample_dir: &Vector,
+        u0: f32,
+        u1: f32,
+        pass_through_event: f32,
+        pdfw: f32,
+        event: &BSDFEvent,
+    ) -> Spectrum {
+        todo!()
+    }
+
+    fn pdf(
+        &self,
+        hp: &HitPoint,
+        local_light_dir: &Vector,
+        local_eye_dir: &Vector,
+        direct_pd_fw: f32,
+        reverse_pd_fw: f32,
+    ) {
+        todo!()
+    }
+
+    fn add_referenced_textures(&mut self, v: &Vec<Texture>) {
+        todo!()
+    }
+
+    fn update_texture_references(&mut self, old_tex: &Texture, new_tex: &Texture) {
+        todo!()
+    }
+
+    fn to_properties(&self, imc: ImageMapCache, real_filename: bool) -> Properties {
+        todo!()
+    }
+}
--- src/slg/material/mod.rs
+++ src/slg/material/mod.rs
@@ -4,8 +4,14 @@ pub use self::definitions::MaterialDefinitions;
 mod material;
 pub use self::material::Material;
 
+mod arch_glass;
 mod glass;
+pub use self::arch_glass::ArchGlassMaterial;
 pub use self::glass::GlassMaterial;
 
 mod mirror;
 pub use self::mirror::MirrorMaterial;
+
+mod matte;
+
+pub use self::matte::MatteMaterial;
